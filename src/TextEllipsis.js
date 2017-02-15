0b55dd549d5b6251687988077f968148ed1f4085
mahui.mh <mahui.mh@alibaba-inc.com>
2017-02-15 22:53:33 +0800

    重新修改计算逻辑，添加渐隐效果，丰富demo

--- src/TextEllipsis.js
+++ src/TextEllipsis.js
@@ -6,183 +6,87 @@
  * All rights reserved.
  */
 import React from 'react';
-import { getWidthFromDOM } from './utils';
+import ReactDOM from 'react-dom';
+import { getHeightFromDOM } from './utils';
 import Tooltip from 'uxcore-tooltip';
 
 class TextEllipsis extends React.Component {
 
   constructor(props) {
     super(props);
-    // 判断是否支持 WebkitLineClamp 属性
-    let isSupportLineClamp = true;
-    const node = document.createElement('div');
-
-    if ('WebkitLineClamp' in node.style) {
-      node.style['WebkitLineClamp'] = 3;
-      if (node.style['WebkitLineClamp'] != 3) {
-        isSupportLineClamp = false;
-      }
-    } else {
-      isSupportLineClamp = false;
-    }
 
     this.state = {
-      isSupportLineClamp,
-      wrapWidth: 'auto',
-      fontSize: 16,
+      textLineHeight: 16,
+      height: 'auto',
     };
   }
 
   componentDidMount() {
-    let wrapDOM = ReactDOM.findDOMNode(this).parentNode;
-    let wrapWidth = getWidthFromDOM(wrapDOM);
-    // 拿到父结构的 font-size 用于自动计算宽度
-    let fontSize = wrapDOM.currentStyle ? parseInt(wrapDOM.currentStyle.fontSize) : parseInt(window.getComputedStyle(wrapDOM, null).getPropertyValue('font-size'));
+    const wrapDOM = ReactDOM.findDOMNode(this).parentNode;
+    const praHeight = getHeightFromDOM(wrapDOM);
+    // 拿到父结构的 line-height 用于自动计算高度
+    const praLineHeight = wrapDOM.currentStyle ? parseInt(wrapDOM.currentStyle.lineHeight, 10) :
+                   parseInt(window.getComputedStyle(wrapDOM, null)
+                   .getPropertyValue('line-height'), 10);
 
     this.setState({
-      wrapWidth: wrapWidth,
-      fontSize: fontSize,
+      height: praHeight,
+      textLineHeight: praLineHeight,
     });
   }
 
   render() {
-    let content = null;
-    let { style = null, prefixCls, className = '', overlayClassName, lineLimit, text } = this.props;
-    const { wrapWidth, isSupportLineClamp, fontSize } = this.state;
+    const { prefixCls, className = '', overlayClassName, showTooltip, lineLimit, text }
+          = this.props;
+    const { height, textLineHeight } = this.state;
 
-    let cls = prefixCls || "uxcore-text-ellipsis";
-    if(className){
-      cls += ' ' + className;
+
+    let cls = prefixCls || 'uxcore-text-ellipsis';
+    if (className) {
+      cls = `${cls} text ${className}`;
     }
 
-    if (lineLimit === 1) {
+    let textEllipsisContent = null;
+    let handlerDom = <span className="handler">...</span>;
+    let wrapperStyle = { maxHeight: lineLimit * textLineHeight };
 
-      content = (
-        <span
-          className={cls}
-          style={{
-            width: wrapWidth,
-            textOverflow: 'ellipsis',
-            display: 'inline-block',
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            ...style,
-          }}
+    if (lineLimit && (lineLimit * textLineHeight) < height) {
+      if (showTooltip) {
+        handlerDom = (<Tooltip
+          overlayClassName={overlayClassName}
+          overlay={text}
         >
-          {text}
-        </span>
-      );
-
-    } else if (lineLimit > 1) {
-
-      if (this.state.isSupportLineClamp) {
-
-        content = (
-          <span
-            className={cls}
-            style={{
-              width: wrapWidth,
-              textOverflow: 'ellipsis',
-              display: 'inline-block',
-              overflow: 'hidden',
-              display: '-webkit-box',
-              WebkitLineClamp: lineLimit,
-              WebkitBoxOrient: 'vertical',
-              ...style,
-            }}
-          >
-            {text}
-          </span>
-        );
-
-      } else {
-
-        let lineCount = 10;
-        
-        if (wrapWidth !== 'auto') {
-          lineCount = parseFloat(wrapWidth / fontSize, 10);
-        }
-
-        lineCount = Math.floor(lineCount);
-
-        let textArr = getTextArr(text, lineCount, lineLimit);
-
-        const textList = textArr.map((item, index)=> {
-
-          // 最后一个超过一行长度的裁切一下加下省略号
-          if (index == lineLimit - 1 && item.length === lineCount) {
-            return <span key={index}>{setEllipsis(item)}</span>;
-          }
-
-          return <span key={index}>{item}</span>;
-
-        });
-
-        content = (
-          <span
-            className={cls}
-            style={{
-              width: wrapWidth,
-              ...style
-            }}
-          >
-            {textList}
-          </span>
-        );
+          <span className="handler">...</span>
+        </Tooltip>);
       }
-
-    }
-
-
-    if (this.props.showTooltip) {
-      return (
-        <Tooltip overlayClassName={overlayClassName} overlay={text} placement="bottom">
-         {content}
-        </Tooltip>
-      );
+      textEllipsisContent = (<span className="textContainer" style={wrapperStyle}>
+        <span className={cls}>{text}</span>
+        <span
+          className="check-all"
+          style={{ height: `${textLineHeight}px`, lineHeight: `${textLineHeight}px` }}
+        >
+          <span className="dot-more" style={{ height: `${textLineHeight}px` }}></span>
+          {handlerDom}
+        </span>
+      </span>);
     } else {
-      return (
-        <span title={text}>{content}</span>
-      );
+      textEllipsisContent = <span>{text}</span>;
     }
+
+    return textEllipsisContent;
   }
 }
 
-function getTextArr(text, lineTextLength, lineLimit) {
-  let result = [];
-
-  for (let i = 1; i <= Math.ceil(text.length/lineTextLength); i++) {
-    const start = lineTextLength * (i - 1);
-    const end = Math.min(i * lineTextLength, text.length);
-    const currentStr = text.substring(start, end);
-    result.push(currentStr);
-  }
-
-  if (result.length > lineLimit) {
-    // 兼容 IE8 ，splice方法必须要有第二个参数
-    result.splice(lineLimit,result.length-lineLimit);
-  }
-
-  return result;
-}
-
-function setEllipsis(text) {
-  let textArr = text.split('');
-  textArr.splice(textArr.length - 1, 3, '...');
-  return textArr.join('');
-}
 
 TextEllipsis.defaultProps = {
   className: '',
   overlayClassName: '',
-  lineLimit: 1,
   showTooltip: false,
   // 设置内容
   text: '',
 };
 
 TextEllipsis.propTypes = {
-  style: React.PropTypes.object,
   prefixCls: React.PropTypes.string,
   overlayClassName: React.PropTypes.string,
   className: React.PropTypes.string,
--- src/utils.js
+++ src/utils.js
@@ -1,54 +1,16 @@
 export default {
-  throttle: (func, wait, options) => {
-    // from underscore
-    var context, args, result;
-    var timeout = null;
-    var previous = 0;
-    if (!options) options = {};
-    var later = function () {
-      previous = options.leading === false ? 0 : new Date().getTime();
-      timeout = null;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    };
-    return function () {
-      var now = new Date().getTime();
-      if (!previous && options.leading === false) previous = now;
-      var remaining = wait - (now - previous);
-      context = this;
-      args = arguments;
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply(context, args);
-        if (!timeout) context = args = null;
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-      return result;
-    };
-  },
-  getWidthFromDOM: (dom) => {
+  getHeightFromDOM: (dom) => {
     const styles = dom.currentStyle ? dom.currentStyle : window.getComputedStyle(dom);
-    const width = dom.offsetWidth;
-    const borderLeftWidth = isNaN(parseFloat(styles.borderLeftWidth)) ?
-                            0 : parseFloat(styles.borderLeftWidth);
-    const borderRightWidth = isNaN(parseFloat(styles.borderRightWidth)) ?
-                             0 : parseFloat(styles.borderRightWidth);
-    const paddingLeft = isNaN(parseFloat(styles.paddingLeft)) ?
-                        0 : parseFloat(styles.paddingLeft);
-    const paddingRight = isNaN(parseFloat(styles.paddingRight)) ?
-                         0 : parseFloat(styles.paddingRight);
+    const height = dom.offsetHeight;
+    const borderTopWidth = isNaN(parseFloat(styles.borderTopWidth)) ?
+                            0 : parseFloat(styles.borderTopWidth);
+    const borderBottomWidth = isNaN(parseFloat(styles.borderBottomWidth)) ?
+                             0 : parseFloat(styles.borderBottomWidth);
+    const paddingTop = isNaN(parseFloat(styles.paddingTop)) ?
+                        0 : parseFloat(styles.paddingTop);
+    const paddingBottom = isNaN(parseFloat(styles.paddingBottom)) ?
+                         0 : parseFloat(styles.paddingBottom);
 
-    return width - borderLeftWidth - borderRightWidth - paddingLeft - paddingRight;
-  },
-  on: (dom, event, callback) => {
-    dom.addEventListener(event, callback, false);
-  },
-  off: (dom, event, callback) => {
-    dom.removeEventListener(event, callback);
+    return height - borderTopWidth - borderBottomWidth - paddingTop - paddingBottom;
   },
 };
